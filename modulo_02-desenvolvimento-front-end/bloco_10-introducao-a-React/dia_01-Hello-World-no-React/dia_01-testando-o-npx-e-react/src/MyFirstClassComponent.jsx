076890d8cf5ab0abe97c43a5ba923b28f0efb87f
Heloisa <heloisajacobi@gmail.com>
2022-01-22 13:30:11 -0300

    Módulo_02-Desenvolvimento-Front-End

---
+++ modulo_02-desenvolvimento-front-end/bloco_10-introducao-a-React/dia_01-Hello-World-no-React/dia_01-testando-o-npx-e-react/src/MyFirstClassComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ReactClass extends React.Component {
+  render() {
+    return (
+      <h2>My first React Class Component!</h2>
+    )
+  }
+}
+
+export default ReactClass;
